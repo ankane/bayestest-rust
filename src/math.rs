af637f6785acfe9234686fe03d493d8b2e162ba5
Andrew Kane <andrew@ankane.org>
2022-07-30 18:17:12 -0700

    Reduced dependencies

--- src/math.rs
+++ src/math.rs
@@ -1,7 +1,10 @@
-use libm::lgamma_r;
+extern "C" {
+    pub fn lgamma_r(x: f64, signp: *mut i32) -> f64;
+}
 
 pub fn loggamma(x: f64) -> f64 {
-    lgamma_r(x).0
+    let mut signp = 0;
+    unsafe { lgamma_r(x, &mut signp) }
 }
 
 pub fn logbeta(a: f64, b: f64) -> f64 {
